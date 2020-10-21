da600b7612b8934f53e647b5e69d03f69ed757e2
P-EW <paul-emile.watelot3@etu.univ-lorraine.fr>
2020-10-21 15:32:23 +0200

    Ajout de la classe FileReader et d'un premier labyrinthe.

--- src/main/java/model/Labyrinthe.java
+++ src/main/java/model/Labyrinthe.java
@@ -4,4 +4,14 @@ package model;
  * @author Tabary
  */
 public class Labyrinthe {
+
+    private int[][] plateau;
+
+    public Labyrinthe(){
+    }
+
+    public Labyrinthe(int[][] plateau){
+        this.plateau = plateau;
+    }
+
 }
